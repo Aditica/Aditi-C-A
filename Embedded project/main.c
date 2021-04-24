69a978fa4406e298893d749e2fddf7b75ef84cf8
Aditica <80763932+Aditica@users.noreply.github.com>
2021-04-24 22:59:37 +0530

    Update main.c

--- Embedded project/main.c
+++ Embedded project/main.c
@@ -1,1 +1,39 @@
 
+#include<stdio.h>
+void Butt(),HeaterS(),Ledop();
+void Butt()
+{
+PD0=0x00;
+PB0=0x00;
+if(PD1==0)
+{
+PB0=0;
+}
+else
+{
+PB0=1;
+}
+}
+void HeaterS()
+{
+PD1=0x00;
+PB1=0x00;
+if(PD1==0)
+{
+PB1=0;
+}
+else
+{
+PB1=1;
+}
+}
+void Ledop()
+{
+PB2=0x00;
+if(PD0||PD1==0)
+PB2==0;
+else
+{
+PB2=1;
+}
+}
